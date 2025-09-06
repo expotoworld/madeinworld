9cdd76c9c8a50eb2a9a599c170e6db9826bee129
sole-madeinworld <expotobsrl@gmail.com>
2025-09-06 20:21:18 +0200

    terraform: reuse existing CE SERVICE anomaly monitor if present (avoid limit exceeded)

--- terraform/app_runner/anomaly_detection.tf
+++ terraform/app_runner/anomaly_detection.tf
@@ -1,11 +1,18 @@
 // AWS Cost Anomaly Detection for Amazon CloudWatch service
 // Note: Cost Explorer/Anomaly Detection API is in us-east-1
 
+# If an existing DIMENSIONAL SERVICE monitor ARN is provided, skip creating a new monitor
 resource "aws_ce_anomaly_monitor" "cloudwatch_service" {
+  count             = var.ce_monitor_arn == "" ? 1 : 0
   provider          = aws.us_east_1
   name              = "cloudwatch-service-monitor"
   monitor_type      = "DIMENSIONAL"
   monitor_dimension = "SERVICE"
+}
+
+# Resolve the monitor ARN (existing via variable or newly created by this stack)
+locals {
+  resolved_ce_monitor_arn = var.ce_monitor_arn != "" ? var.ce_monitor_arn : (length(aws_ce_anomaly_monitor.cloudwatch_service) > 0 ? aws_ce_anomaly_monitor.cloudwatch_service[0].arn : "")
 }
 
 resource "aws_ce_anomaly_subscription" "cloudwatch_alerts" {
@@ -13,7 +20,7 @@ resource "aws_ce_anomaly_subscription" "cloudwatch_alerts" {
   name      = "cloudwatch-anomaly-alerts"
   frequency = "DAILY"
 
-  monitor_arn_list = [aws_ce_anomaly_monitor.cloudwatch_service.arn]
+  monitor_arn_list = [local.resolved_ce_monitor_arn]
 
   threshold_expression {
     and {
@@ -30,4 +37,3 @@ resource "aws_ce_anomaly_subscription" "cloudwatch_alerts" {
     address = "expotobsrl@gmail.com"
   }
 }
-
